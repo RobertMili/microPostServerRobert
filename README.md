22792dba6dad20ca00d13f713629bb3b27f2ab29
RobertMili <robertmilicevic3869@gmail.com>
2023-10-23 19:14:24 +0200

    update README.md

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # microPostServerRobert
 
 
-Service to handle messages. Saves to Mysql and using RabbitMQ.
+Service to handle (send and receive) messages. 
+
+Using:
+- Mysql
+- RabbitMQ.
 
 Endpoints:
 POST:
@@ -11,13 +15,20 @@ POST:
 
 */posts*
 
-- Except Json in the format;   
+Except Json in the format: 
+
  {   
+
 	"id": 2,
+
 	"fromUser": "asd",
+
 	"toUserName": "martin",
+
 	"message": "This is a message",
+
 	"dataAndTime": "2023-10-22T16:17:14.273310099"
+
 }
 
 
@@ -32,8 +43,10 @@ returns 200 to check if server is up
 *  Header must include sender userID 
 * Return a Json if the requested id:
 
-* {  
+
+  
 {
+
 	"id": 2,
 	"fromUser": "asd",
 	"toUserName": "martin",
--- src/main/java/com/example/micropostserverrobert/MicroPostServerRobertApplication.java
+++ src/main/java/com/example/micropostserverrobert/MicroPostServerRobertApplication.java
@@ -1,31 +1,16 @@
 package com.example.micropostserverrobert;
 
-
-
-import org.springframework.beans.factory.annotation.Autowired;
-
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-public class MicroPostServerRobertApplication implements CommandLineRunner {
-
-//
-//    @Autowired
-//    private RabbitConnection rabbitMQConsumer;
+public class MicroPostServerRobertApplication {
 
 
     public static void main(String[] args) {
         SpringApplication.run(MicroPostServerRobertApplication.class, args);
 
-
-
     }
 
-    @Override
-    public void run(String... args) throws Exception {
-
-    }
 }
